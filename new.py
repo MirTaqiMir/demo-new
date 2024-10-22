9d73f30a9a721dc161429af0d1b34b4535547126
Mir Taqi Mir <91200793+MirTaqiMir@users.noreply.github.com>
2024-10-22 17:25:04 +0500

    print

---
+++ new.py
@@ -0,0 +1,9 @@
+# Input: Getting two numbers from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Adding the two numbers
+sum = num1 + num2
+
+# Output: Displaying the result
+print("The sum of", num1, "and", num2, "is:", sum)
